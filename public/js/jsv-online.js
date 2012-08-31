9e82109fbd3290ba86762198c996135005e52070
Leonardo Gomes <leonardo.f.gomes@gmail.com>
2012-08-31 02:59:49 +0200

    Added dynamic status messages.

--- public/js/jsv-online.js
+++ public/js/jsv-online.js
@@ -10,6 +10,21 @@ var jsonEditor = CodeMirror.fromTextArea($('#json')[0], options);
 schemaEditor.focus();
 //schemaEditor.setSelection({ line : 0, ch : 0}, {line : 0, ch : 23});
 
+var processResult = function( data ){
+	if (data.isValid) {
+		$('#resultMessage').html("<div class='alert alert-success span6'>" +
+			"<button type='button' class='close' data-dismiss='alert'>×</button>" +
+	  		"<strong>Good job!</strong> Your JSON is valid.</div>");
+		//.html("&lt;div class=&quot;alert alert-success span6&quot;&gt;<br/>	  		&lt;button type=&quot;button&quot; class=&quot;close&quot; data-dismiss=&quot;alert&quot;&gt;&#x00d7;&lt;/button&gt;<br/>	  		&lt;strong&gt;Good job!&lt;/strong&gt; Your JSON is valid.<br/>		 &lt;/div&gt;");
+
+	} else {
+		$('#resultMessage').html("<div class=\"alert alert-error span6\">" +
+	  		"<button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>" +
+	  		"<strong>Warning!</strong> Better check your JSON.</div>");
+	}
+
+};
+
 $('button[data-action=validate]').bind('click', function () {
   $.get("validate", 
   		{ 
@@ -17,7 +32,7 @@ $('button[data-action=validate]').bind('click', function () {
   			json : jsonEditor.getValue(" ")
   		},
 			function(data){
-				console.log(data);
+				processResult(data);
 			}, "json");
 });
 
